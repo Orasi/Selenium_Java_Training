6bb9e50841929f3b322c80be685d83b631ae8d4c
waitsavery <wwavery0352@yahoo.com>
2015-09-22 10:04:57 -0400

    added more training modules

--- src/test/java/TODO_14_Actions/_01_Actions.java
+++ src/test/java/TODO_14_Actions/_01_Actions.java
@@ -1,15 +1,12 @@
 package TODO_14_Actions;
 
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
